61e57483827459b87f461909deebd9a40ad6938d
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2019-02-06 18:48:29 +0300

    DEV-38115. Remove unused class

--- lib/app/utils/ui_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class UIColors {
-  static Color redTask = Colors.red[700];
-  static Color yellowTask = Colors.yellow[700];
-  static Color greenTask = Colors.green[700];
-  static Color normalTask = Colors.grey[700];
-}
