e386edbcb23d055baecd911d79f6a1ebcfd40f06
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2019-02-06 18:47:11 +0300

    DEV-38115. Fix Search page

---
+++ lib/app/pages/catalog/search_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+import 'package:selvis_flutter/app/models/product.dart';
+import 'package:selvis_flutter/app/pages/product_page.dart';
+
+class SearchPage extends StatefulWidget {
+  SearchPage({Key key}) : super(key: key);
+
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final TextEditingController _queryTextController = TextEditingController();
+  List<Product> _products;
+
+  Widget _buildProductList(BuildContext context) {
+    if (_products.length == 0) {
+      return Center(child: Text('Ничего не найдено', textAlign: TextAlign.center));
+    }
+
+    return ListView(
+      children: _products.map((Product product) {
+        return ListTile(
+          leading: SizedBox(
+            child: product.image,
+            width: 48,
+            height: 52
+          ),
+          isThreeLine: false,
+          title: Text(product.wareName, style: Theme.of(context).textTheme.caption),
+          onTap: () {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => ProductPage(product: product))
+            );
+          }
+        );
+      }).toList()
+    );
+  }
+
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          autocorrect: false,
+          controller: _queryTextController,
+          style: theme.primaryTextTheme.title,
+          textInputAction: TextInputAction.search,
+          onChanged: (String val) => setState((){}),
+          onSubmitted: (String searchStr) async {
+            _products = await Product.loadByName(searchStr);
+            setState(() {});
+          },
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Поиск',
+            hintStyle: theme.primaryTextTheme.title,
+          ),
+        ),
+        actions: _buildActions(context),
+      ),
+      body: _products != null ? _buildProductList(context) : Container()
+    );
+  }
+
+  List<Widget> _buildActions(BuildContext context) {
+    return <Widget>[
+      _queryTextController.text.isEmpty ?
+        Container() :
+        IconButton(icon: Icon(Icons.clear), onPressed: _reset)
+      ];
+  }
+
+  void _reset() {
+    _queryTextController.text = '';
+    _products = null;
+    setState(() {});
+  }
+}
--- lib/app/pages/catalog_page.dart
+++ lib/app/pages/catalog_page.dart
@@ -8,6 +8,7 @@ import 'package:selvis_flutter/app/app.dart';
 import 'package:selvis_flutter/app/models/group.dart';
 import 'package:selvis_flutter/app/models/product.dart';
 import 'package:selvis_flutter/app/modules/api.dart';
+import 'package:selvis_flutter/app/pages/catalog/search_page.dart';
 import 'package:selvis_flutter/app/pages/sub_groups_page.dart';
 import 'package:selvis_flutter/app/pages/product_page.dart';
 import 'package:selvis_flutter/app/widgets/api_page_widget.dart';
@@ -20,7 +21,6 @@ class CatalogPage extends StatefulWidget {
 }
 
 class _CatalogPageState extends State<CatalogPage> with WidgetsBindingObserver {
-  _CatalogPageDelegate _delegate = _CatalogPageDelegate();
   GlobalKey<ApiPageWidgetState> _apiWidgetKey = GlobalKey();
   List<Group> _groups = [];
 
@@ -52,7 +52,7 @@ class _CatalogPageState extends State<CatalogPage> with WidgetsBindingObserver {
           child: Column(
             children: <Widget>[
               SizedBox(
-                child: Image.network(App.application.config.apiBaseUrl + 'images/source/groups/${group.title}'),
+                child: group.image,
                 height: 112,
                 width: 112
               ),
@@ -74,7 +74,7 @@ class _CatalogPageState extends State<CatalogPage> with WidgetsBindingObserver {
   }
 
   void _searchProduct() async {
-    await showSearch<Product>(context: context, delegate: _delegate);
+    Navigator.push(context, MaterialPageRoute(builder: (context) => SearchPage()));
   }
 
   void _scanBarcode() async {
@@ -111,72 +111,5 @@ class _CatalogPageState extends State<CatalogPage> with WidgetsBindingObserver {
       buildBody: _buildBody,
       loadData: _loadData,
     );
-  }
-}
-
-
-class _CatalogPageDelegate extends SearchDelegate<Product> {
-  List<Product> _products;
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      tooltip: 'Назад',
-      icon: AnimatedIcon(
-        icon: AnimatedIcons.menu_arrow,
-        progress: transitionAnimation,
-      ),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return Container();
-  }
-
-  Future<void> _loadSuggestions() async {
-    String searchStr = query.toLowerCase();
-    if (searchStr != '') {
-      _products = await Product.loadByName(searchStr);
-    }
-  }
-
-  void _reset() {
-    query = '';
-    _products = null;
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    _loadSuggestions();
-
-    if (_products != null && _products.isEmpty) {
-      return Center(child: Text('Ничего не найдено', textAlign: TextAlign.center));
-    }
-
-    return ListView(
-      children: (_products ?? []).map((Product product) {
-        return ListTile(
-          leading: SizedBox(
-            child: product.image,
-            width: 48,
-            height: 52
-          ),
-          isThreeLine: false,
-          title: Text(product.wareName, style: Theme.of(context).textTheme.caption),
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => ProductPage(product: product)));
-          }
-        );
-      }).toList()
-    );
-  }
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return <Widget>[query.isEmpty ? Container() : IconButton(icon: Icon(Icons.clear), onPressed: _reset)];
   }
 }
