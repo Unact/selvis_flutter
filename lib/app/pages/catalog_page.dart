9feb233b7d2a0ff723992d12128b33f7e33cf1e0
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2019-02-14 17:51:34 +0300

    DEV-38169. Fix search

--- lib/app/pages/catalog_page.dart
+++ lib/app/pages/catalog_page.dart
@@ -3,12 +3,13 @@ import 'dart:async';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 import 'package:selvis_flutter/app/app.dart';
 import 'package:selvis_flutter/app/models/group.dart';
 import 'package:selvis_flutter/app/models/product.dart';
 import 'package:selvis_flutter/app/modules/api.dart';
-import 'package:selvis_flutter/app/pages/catalog/search_page.dart';
 import 'package:selvis_flutter/app/pages/sub_groups_page.dart';
 import 'package:selvis_flutter/app/pages/product_page.dart';
 import 'package:selvis_flutter/app/widgets/api_page_widget.dart';
@@ -21,60 +22,130 @@ class CatalogPage extends StatefulWidget {
 }
 
 class _CatalogPageState extends State<CatalogPage> with WidgetsBindingObserver {
+  final TextEditingController _queryTextController = TextEditingController();
   GlobalKey<ApiPageWidgetState> _apiWidgetKey = GlobalKey();
   List<Group> _groups = [];
 
   AppBar _buildAppBar(BuildContext context) {
     return AppBar(
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.search),
-          onPressed: _searchProduct
-        ),
-        IconButton(
-          color: Colors.white,
-          icon: Icon(Icons.camera_alt),
-          onPressed: _scanBarcode
-        ),
-      ],
+      centerTitle: false,
       title: Text(App.application.config.packageInfo.appName)
     );
   }
 
-  Widget _buildBody(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 2,
-      children: _groups.map((Group group) {
-        return GestureDetector(
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => SubGroupsPage(parentGroup: group)));
-          },
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                child: group.image,
-                height: 112,
-                width: 112
-              ),
-              Text(
-                group.title,
-                style: Theme.of(context).textTheme.subtitle,
-                textAlign: TextAlign.center
-              )
-            ]
+  Widget _buildHeader(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+
+    return Container(
+      color: theme.bottomAppBarColor,
+      child: TypeAheadField(
+        textFieldConfiguration: TextFieldConfiguration(
+          cursorColor: theme.textSelectionColor,
+          autocorrect: false,
+          controller: _queryTextController,
+          textInputAction: TextInputAction.search,
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.search),
+            suffixIcon: IconButton(
+              icon: Icon(Icons.camera_alt),
+              onPressed: _scanBarcode
+            ),
+            border: InputBorder.none,
+            hintText: 'Поиск'
           )
-        );
-      }).toList()
+        ),
+        errorBuilder: (BuildContext ctx, error) {
+          return Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Произошла ошибка',
+              style: TextStyle(color: theme.errorColor),
+            ),
+          );
+        },
+        noItemsFoundBuilder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Ничего не найдено',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: theme.disabledColor, fontSize: 14.0),
+            ),
+          );
+        },
+        suggestionsCallback: (String value) async {
+          return value.isNotEmpty ? await Product.loadByName(value) : [];
+        },
+        itemBuilder: (BuildContext ctx, suggestion) {
+          return ListTile(
+            leading: SizedBox(
+              child: suggestion.image,
+              width: 48,
+              height: 52
+            ),
+            isThreeLine: false,
+            title: Text(suggestion.wareName, style: Theme.of(context).textTheme.caption)
+          );
+        },
+        onSuggestionSelected: (suggestion) async {
+          await Navigator.of(context, rootNavigator: true).push(
+            MaterialPageRoute(builder: (context) => ProductPage(product: suggestion))
+          );
+        }
+      )
+    );
+  }
+
+  Widget _buildSliver(BuildContext context) {
+    return SliverGrid(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 4.0,
+        mainAxisSpacing: 4.0
+      ),
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int idx) {
+          Group group = _groups[idx];
+
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => SubGroupsPage(parentGroup: group)));
+            },
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  child: group.image,
+                  height: 112,
+                  width: 112
+                ),
+                Text(
+                  group.title,
+                  style: Theme.of(context).textTheme.subtitle,
+                  textAlign: TextAlign.center
+                )
+              ]
+            )
+          );
+        },
+        childCount: _groups.length
+      )
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverStickyHeader(
+          header: _buildHeader(context),
+          sliver: _buildSliver(context)
+        )
+      ]
     );
   }
 
   Future<void> _loadData() async {
     List<Group> topGroups = await Group.loadFromRemote();
     _groups = topGroups.map((group) => group.childrenList).expand((el) => el).toList();
-  }
-
-  void _searchProduct() async {
-    Navigator.of(context, rootNavigator: true).push(MaterialPageRoute(builder: (context) => SearchPage()));
   }
 
   void _scanBarcode() async {
@@ -110,6 +181,7 @@ class _CatalogPageState extends State<CatalogPage> with WidgetsBindingObserver {
     return ApiPageWidget(
       key: _apiWidgetKey,
       buildAppBar: _buildAppBar,
+      bodyWithPadding: false,
       buildBody: _buildBody,
       loadData: _loadData,
     );
